7166b0a622f9b8bafa35ffc3e93238e14fb9ef5a
Franceline Casanas <franceline90@gmail.com>
2020-05-28 02:11:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
-end
+# def countdown
+# end
+
+# def countdown(number)
+#   number = 10
+#   while number <= 10
+#     puts "#{number} SECOND(S)!"
+#     number -= 1
+#   end
+# end
